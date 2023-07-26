d9e1493e5e76a148e82e6b7e41117a8e9bbe9a9b
sean-bailey <seanbailey518@gmail.com>
2023-07-26 15:38:32 -0400

    now with side-by-side testing enmasse

---
+++ llama-anywhere/mass_test.py
@@ -0,0 +1,41 @@
+import argparse
+import subprocess
+import boto3
+
+def list_instance_types(region_name):
+    ec2 = boto3.client('ec2', region_name=region_name)
+    instance_types = []
+
+    paginator = ec2.get_paginator('describe_instance_types')
+    for page in paginator.paginate():
+        for instance_type in page['InstanceTypes']:
+            if instance_type['MemoryInfo']['SizeInMiB'] >= 6 * 1024: #Based on my testing with Lambda, it's going to be useless to even try the quantized models on architecture smaller than 6GB.
+                instance_types.append(instance_type['InstanceType'])
+
+    return instance_types
+
+def run_shell_script(deploytype, port, model, instance_type):
+    command = ["bash", "end2endtest.sh", 
+               "-deploytype", deploytype, 
+               "-port", str(port), 
+               "-model", model, 
+               "-instancetype", instance_type]
+    subprocess.check_call(command)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("foundationmodel",type=str,default="VMware/open-llama-7b-v2-open-instruct", help="Huggingface transformer compatible repository path (VMware/open-llama-7b-v2-open-instruct)")
+    parser.add_argument("quantizedmodel", type=str,default="https://huggingface.co/TheBloke/open-llama-7B-v2-open-instruct-GGML/resolve/main/open-llama-7b-v2-open-instruct.ggmlv3.q2_K.bin",help="URL or path to the quantized model (https://huggingface.co/TheBloke/open-llama-7B-v2-open-instruct-GGML/resolve/main/open-llama-7b-v2-open-instruct.ggmlv3.q2_K.bin)")
+    args = parser.parse_args()
+    port = 8080
+    instance_types = list_instance_types('us-east-1') 
+
+    for instance_type in instance_types:
+        # Run shell script for foundation model
+        run_shell_script('f', port, args.foundationmodel, instance_type)
+
+        # Run shell script for quantized model
+        run_shell_script('q', port, args.quantizedmodel, instance_type)
+
+if __name__ == '__main__':
+    main()
