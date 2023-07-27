d07dd9b9bcdda9b96f54779efb52b5f395e119d2
sean-bailey <seanbailey518@gmail.com>
2023-07-27 13:49:26 -0400

    debugging

--- foundational_container/prepare_huggingface_data.py
+++ foundational_container/prepare_huggingface_data.py
@@ -1,13 +1,21 @@
 from transformers import AutoModel, AutoTokenizer, pipeline
 import os
+import traceback
 
 
 
 
-SAVEPATH=os.environ.get("SAVE_PATH")
-MODEL = os.environ.get("MODEL_TYPE")
+try:
 
-#pipe=pipeline(task="text-generation",model=MODEL, trust_remote_code=True)
-tokenizer = AutoTokenizer.from_pretrained(MODEL,cache_dir=SAVEPATH)
-model = AutoModelForCausalLM.from_pretrained(MODEL,cache_dir=SAVEPATH)
-#I'm hoping that this will download to the appropriate local directory and it will check there first. Who knows?
+    SAVEPATH=os.environ.get("SAVE_PATH")
+    MODEL = os.environ.get("MODEL_TYPE")
+
+    print(SAVEPATH)
+    print(MODEL)
+
+    #pipe=pipeline(task="text-generation",model=MODEL, trust_remote_code=True)
+    tokenizer = AutoTokenizer.from_pretrained(MODEL,cache_dir=SAVEPATH)
+    model = AutoModelForCausalLM.from_pretrained(MODEL,cache_dir=SAVEPATH)
+    #I'm hoping that this will download to the appropriate local directory and it will check there first. Who knows?
+except:
+    print(traceback.format_exc())
